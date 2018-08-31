c7404046ca8a188d170179af4a0a397f2dbbaba6
Efraim Marcatto <efraimmarcatto@gmail.com>
2018-08-31 15:11:45 -0300

    mudança dominio

--- facelogin.php
+++ facelogin.php
@@ -4,7 +4,7 @@ $helper = $fb->getRedirectLoginHelper();
 
 $permissions = array('email');
 
-$loginurl = $helper->getLoginUrl('http://localhost/quiz/php/callback.php', $permissions);
+$loginurl = $helper->getLoginUrl('http://phpfinal.herokuapp.com/callback.php', $permissions);
 
 
 ?>
