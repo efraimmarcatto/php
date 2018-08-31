91e5592d8b70342b2191e64e6f1dd8a5e7866e8c
Efraim Marcatto <efraimmarcatto@gmail.com>
2018-08-31 14:18:25 -0300

    alterando para postgres

--- con.php
+++ con.php
@@ -1,13 +1,11 @@
 <?php
 
-$dsn = "mysql:dbname=quizdb;host=localhost";
-$dbuser = "root";
-$dbpass = "senha123";
+$dsn = "pgsql:host=ec2-54-235-86-226.compute-1.amazonaws.com dbname=d67e3i82456bvo user=wpqjoarmffoamb password=c73b3f22bba344b9c52d1185039407eec46eda836ffe2bbfa022104a08ebc9cb";
 
 
 try{
 
-	$pdo = new PDO($dsn,$dbuser,$dbpass);
+	$pdo = new PDO($dsn);
 }catch(PDOexception $e){
 
 	echo "Falhou a conexão".$e->getMessage();
