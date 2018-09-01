f9c637167466f336bd07887ec1e621c6900f55df
Efraim Marcatto <efraimmarcatto@gmail.com>
2018-09-01 15:23:56 -0300

    sql

--- votar.php
+++ votar.php
@@ -24,7 +24,7 @@ if(isset($_SESSION['fb_access_token']) && !empty($_SESSION['fb_access_token']))
 				
 					$sql = "UPDATE projetos SET votos = votos  + 1 WHERE id = $idp";
 					$sql = $pdo->query($sql);
-					$sql = "INSERT INTO convotos (id_proj,id_uface VALUES ($idp, id_uface='$idfu')";
+					$sql = "INSERT INTO convotos (id_proj,id_uface) VALUES ($idp, '$idfu')";
 					$sql = $pdo->query($sql);
 					header("Location: index.php");
 				
@@ -36,7 +36,7 @@ if(isset($_SESSION['fb_access_token']) && !empty($_SESSION['fb_access_token']))
 		}else{
 					$sql = "UPDATE projetos SET votos = votos  + 1 WHERE id = $idp";
 					$sql = $pdo->query($sql);
-					$sql = "INSERT INTO convotos (id_proj,id_uface VALUES ($idp, id_uface='$idfu')";
+					$sql = "INSERT INTO convotos (id_proj,id_uface) VALUES ($idp, '$idfu')";
 					$sql = $pdo->query($sql);
 										
 					header("Location: index.php");
