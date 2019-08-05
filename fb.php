ef6bc1f0315de71155d3577d05c6413ab9a93170
Efraim Marcatto <efraimmarcatto@gmail.com>
2019-08-05 09:26:18 -0300

    Removendo informações privadas

--- fb.php
+++ fb.php
@@ -3,8 +3,8 @@ session_start();
 require 'Facebook/autoload.php';
 
 $fb = new Facebook\Facebook([
-	'app_id'=> '239101126782820',
-	'app_secret'=> 'd8b39ced34f181e465acbcca700df98c',
+	'app_id'=> '',
+	'app_secret'=> '',
 	'default_graph_version'=>'v3.1'
 ]);
 
